3d6d733160df1ddcd6cd61a457d8a746ca80c6b7
Peter Benc <peter.benc1@gmail.com>
2025-11-25 13:35:07 +0100

    poc: route asset to zcash

---
+++ packages/core/src/types.ts
@@ -0,0 +1,22 @@
+import { SendDepositFn } from "./swapApi/domain"
+
+export type RouteAsset = {
+  blockchain: 'solana'
+  tokenId: string
+}
+
+type AccountBase = {
+  asset: RouteAsset
+  getAddress: () => Promise<string>
+  assetToBaseUnits: (amount: string) => bigint
+}
+
+export type AccountAddressOnly = AccountBase & {
+  type: 'addressOnly'
+}
+
+export type AccountFull = AccountBase & {
+  type: 'full'
+  getBalance: () => Promise<bigint>
+  sendDeposit: SendDepositFn
+}
