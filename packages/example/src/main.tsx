ed88aea40169e2f6cc459a7e917617244b01249c
Peter Benc <peter.benc1@gmail.com>
2025-11-28 13:36:53 +0100

    feat: ui with material ui

--- packages/example/src/main.tsx
+++ packages/example/src/main.tsx
@@ -3,7 +3,7 @@ import './polyfills';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.mui';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
